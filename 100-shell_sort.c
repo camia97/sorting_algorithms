5e00dbef2f13442a3e8cd6933379b08e4bd48457
camia97 <3962@holbertonschool.com>
2022-02-15 13:03:25 -0800

    shell sort

---
+++ 100-shell_sort.c
@@ -0,0 +1,33 @@
+#include "sort.h"
+/**
+ * shell_sort - function that sorts an array
+ * @array: array to be sorted
+ * @size: size of the array
+ */
+void shell_sort(int *array, size_t size)
+{
+	size_t gap = 1, i = 0, j = 0;
+	int tmp;
+
+	while (gap < size / 3)
+	{
+		gap = (gap * 3) + 1;
+	}
+	while (gap >= 1)
+	{
+		for (i = gap; i < size; i++)
+		{ 
+			tmp = array[i];
+			for (j = i; j >= gap; j -= gap)
+			{
+				if (tmp < array[j - gap])
+					array[j] = array[j - gap];
+				else
+					break;
+			}
+			array[j] = tmp;
+		}
+		print_array(array, size);
+		gap = (gap - 1) / 3;
+	}
+}
